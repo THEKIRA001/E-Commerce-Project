4c50247766d4e8fa114f9f8fd5795ac1cd2e5ec7
Shubham Bolave <shubhambolave2@gmail.com>
2023-08-12 13:32:52 +0530

    first

--- E-CommerceProject/frontend/src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ E-CommerceProject/frontend/src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-
+import "../HomeSectionCard.css"
 const HomeSectionCard = () => {
   return (
-    <div className="cursor-pointer flex flex-col items-center bg-white rounded-lg overflow-hidden w-[15rem] mx-3">
-      <div className="h-[13rem] w-[10rem]">
+    <div className="cardContainer">
+      <div className="item">
         <img
-          className="object-cover object-top w-full h-full"
+          className="itemImage"
           src="https://rukmini.flixcart.com/image/612/612/xif0q/kurta/j/a/r/l-poch521835-peter-england-original-imag7jg47g7cxhg3-bb.jpeg?q=70"
           alt=""
         />
       </div>
 
-      <div className="p-4">
-        <h3 className="text-lg font-medium text-fray-900">NOfilter</h3>
-        <p className="mt-2 text-sm text-gray-500">Men Solid Pure Cotton Straight Kurta</p>
+      <div className="itemSpace">
+        <h3 className="itemHeading">NOfilter</h3>
+        <p className="itemSubheading">Men Solid Pure Cotton Straight Kurta</p>
       </div>
     </div>
   );
